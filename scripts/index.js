ba92dac409d840275a175ada2fa72255da982e6d
Konstantin Simusev <simussev.k@icloud.com>
2024-05-24 23:20:23 +0500

    feature: added index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,21 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+
+function addCard() {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = 'Hello, World!';
+
+  placesList.append(cardElement);
+}
+
+addCard();
 
 // @todo: Функция удаления карточки
 
